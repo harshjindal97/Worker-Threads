3db3bf4b3c75dbbd991b68be007ecd9dd9e3d36e
Harsh <harsh.j@sustainiam.com>
2025-03-06 09:56:39 +0530

    Initial Commit

---
+++ demo1/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by harsh on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demo1Tests/demo1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  demo1Tests.swift
+//  demo1Tests
+//
+//  Created by harsh on 06/03/25.
+//
+
+import Testing
+
+struct demo1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
